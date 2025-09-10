d6f830766f635788732d17154f7e0ab2036bbed0
Rita Menja <Rita.W.Menja-1@ou.edu>
2025-09-10 14:58:31 -0500

    Homework 1 Sales Finished :)

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Rita Menja
+// Did you seek help ? If yes, specify the helper or web link here: Pooja
 
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,33 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is the product name of the item you are purchasing?");
+            string product = Console.ReadLine();
+            Console.WriteLine("How many Football Ticket's do you want to buy?");
+            string quantity = Console.ReadLine();
+            Console.WriteLine("What is the price for each Football Ticket");
+            string price = Console.ReadLine();
+
+           double Price =  Convert.ToDouble(price);
+           double Quantity = Convert.ToDouble(quantity);
+
+        
+           double subtotal = Price * Quantity;
+
+
+            const double salestax = 0.085;
+            double tax = salestax * subtotal;
+            double total = tax + subtotal;
+
+            Console.WriteLine("Subtotal---" + subtotal);
+
+            Console.WriteLine("Sales Tax---" + tax);
+
+            Console.WriteLine("Total---" + total);
+
+            Console.ReadKey();
+
+
         }
     }
 }
